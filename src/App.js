48da0ebd0ce6fb2706067dca5284a391cad94f8c
arpan13 <arpankumarpal728@gmail.com>
2021-01-15 20:14:03 +0530

    Asked user for giving Permission to Read and Write into Local Storage

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import store from './store';
+import {Provider} from 'react-redux';
+
+import App from './App';
+import {Text} from 'react-native';
+const RootApp = () => {
+  return <Text>Hello form app js</Text>;
+};
+
+export default App;
